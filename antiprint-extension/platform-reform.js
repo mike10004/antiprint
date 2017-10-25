bf9acec01da92d01e97026a6269a320eecd3256b
Mike Chaberski <mchaberski@gmail.com>
2017-10-25 18:30:27 -0400

    add unit tests

--- antiprint-extension/platform-reform.js
+++ antiprint-extension/platform-reform.js
@@ -1071,162 +1071,70 @@
 
 })(typeof window === 'object' ? window : this);
 
-function parseUserAgentSignature(userAgent) {
-  return UAParser(userAgent);
-}
+function PlatformReformer() {
+  let signature;
 
-/**
- * JavaScript Client Detection
- * (C) viazenetti GmbH (Christian Ludwig)
- * https://stackoverflow.com/a/18706818/2657036
- */
-function _parseUserAgentSignature(nAgt) {
-  const unknown = '-';
-  const clientStrings = [
-      {s:'Windows 10', r:/(Windows 10.0|Windows NT 10.0)/},
-      {s:'Windows 8.1', r:/(Windows 8.1|Windows NT 6.3)/},
-      {s:'Windows 8', r:/(Windows 8|Windows NT 6.2)/},
-      {s:'Windows 7', r:/(Windows 7|Windows NT 6.1)/},
-      {s:'Windows Vista', r:/Windows NT 6.0/},
-      {s:'Windows Server 2003', r:/Windows NT 5.2/},
-      {s:'Windows XP', r:/(Windows NT 5.1|Windows XP)/},
-      {s:'Windows 2000', r:/(Windows NT 5.0|Windows 2000)/},
-      {s:'Windows ME', r:/(Win 9x 4.90|Windows ME)/},
-      {s:'Windows 98', r:/(Windows 98|Win98)/},
-      {s:'Windows 95', r:/(Windows 95|Win95|Windows_95)/},
-      {s:'Windows NT 4.0', r:/(Windows NT 4.0|WinNT4.0|WinNT|Windows NT)/},
-      {s:'Windows CE', r:/Windows CE/},
-      {s:'Windows 3.11', r:/Win16/},
-      {s:'Android', r:/Android/},
-      {s:'Open BSD', r:/OpenBSD/},
-      {s:'Sun OS', r:/SunOS/},
-      {s:'Linux', r:/(Linux|X11)/},
-      {s:'iOS', r:/(iPhone|iPad|iPod)/},
-      {s:'Mac OS X', r:/Mac OS X/},
-      {s:'Mac OS', r:/(MacPPC|MacIntel|Mac_PowerPC|Macintosh)/},
-      {s:'QNX', r:/QNX/},
-      {s:'UNIX', r:/UNIX/},
-      {s:'BeOS', r:/BeOS/},
-      {s:'OS/2', r:/OS\/2/},
-      {s:'Search Bot', r:/(nuhk|Googlebot|Yammybot|Openbot|Slurp|MSNBot|Ask Jeeves\/Teoma|ia_archiver)/}
-  ];
-  // browser
-  var browser = unknown;
-  var version = unknown
-  var nameOffset, verOffset, ix;
-
-  // Opera
-  if ((verOffset = nAgt.indexOf('Opera')) != -1) {
-      browser = 'Opera';
-      version = nAgt.substring(verOffset + 6);
-      if ((verOffset = nAgt.indexOf('Version')) != -1) {
-          version = nAgt.substring(verOffset + 8);
-      }
-  }
-  // Opera Next
-  if ((verOffset = nAgt.indexOf('OPR')) != -1) {
-      browser = 'Opera';
-      version = nAgt.substring(verOffset + 4);
-  }
-  // Edge
-  else if ((verOffset = nAgt.indexOf('Edge')) != -1) {
-      browser = 'Microsoft Edge';
-      version = nAgt.substring(verOffset + 5);
-  }
-  // MSIE
-  else if ((verOffset = nAgt.indexOf('MSIE')) != -1) {
-      browser = 'Microsoft Internet Explorer';
-      version = nAgt.substring(verOffset + 5);
-  }
-  // Chrome
-  else if ((verOffset = nAgt.indexOf('Chrome')) != -1) {
-      browser = 'Chrome';
-      version = nAgt.substring(verOffset + 7);
-  }
-  // Safari
-  else if ((verOffset = nAgt.indexOf('Safari')) != -1) {
-      browser = 'Safari';
-      version = nAgt.substring(verOffset + 7);
-      if ((verOffset = nAgt.indexOf('Version')) != -1) {
-          version = nAgt.substring(verOffset + 8);
-      }
-  }
-  // Firefox
-  else if ((verOffset = nAgt.indexOf('Firefox')) != -1) {
-      browser = 'Firefox';
-      version = nAgt.substring(verOffset + 8);
-  }
-  // MSIE 11+
-  else if (nAgt.indexOf('Trident/') != -1) {
-      browser = 'Microsoft Internet Explorer';
-      version = nAgt.substring(nAgt.indexOf('rv:') + 3);
-  }
-  // Other browsers
-  else if ((nameOffset = nAgt.lastIndexOf(' ') + 1) < (verOffset = nAgt.lastIndexOf('/'))) {
-      browser = nAgt.substring(nameOffset, verOffset);
-      version = nAgt.substring(verOffset + 1);
-      if (browser.toLowerCase() == browser.toUpperCase()) {
-          browser = navigator.appName;
-      }
-  }
-  // trim the version string
-  if ((ix = version.indexOf(';')) != -1) version = version.substring(0, ix);
-  if ((ix = version.indexOf(' ')) != -1) version = version.substring(0, ix);
-  if ((ix = version.indexOf(')')) != -1) version = version.substring(0, ix);
-
-  // system
-  let os = unknown;
-  for (const id in clientStrings) {
-      const cs = clientStrings[id];
-      if (cs.r.test(nAgt)) {
-          os = cs.s;
-          break;
-      }
+  function mapArch(arch, osName) {
+    if (osName === 'Linux' && arch === 'amd64') {
+      return 'x86_64';
+    }
+    return arch;
   }
 
-  let osVersion = unknown;
+  const knownLinuxen = ['Ubuntu', 'Debian'];
 
-  if (/Windows/.test(os)) {
-      osVersion = /Windows (.*)/.exec(os)[1];
-      os = 'Windows';
-  }
-
-  switch (os) {
-      case 'Mac OS X':
-          osVersion = /Mac OS X (10[\.\_\d]+)/.exec(nAgt)[1];
-          break;
-
-      case 'Android':
-          osVersion = /Android ([\.\_\d]+)/.exec(nAgt)[1];
-          break;
-
-      case 'iOS':
-          // TODO: derive this from user agent
-          break;
-  }
-
-  return {
-      browser: browser,
-      browserVersion: version,
-      os: os,
-      osVersion: osVersion
+  function constructPlatform(signature) {
+    let osName = signature.os.name || '';
+    if (knownLinuxen.indexOf(osName) !== -1) {
+      osName = 'Linux';
+    }
+    switch (osName) {
+      case 'Windows':
+        return 'Win32';
+      case 'Mac OS':
+        return 'MacIntel';
+      default:
+        return osName + ' ' + mapArch(signature.cpu.architecture, osName);
+    }
   };
+
+  this.parseUserAgentSignature = function (userAgent) {
+    if (typeof signature === 'undefined') {
+      signature = UAParser(userAgent);
+      signature.platform = constructPlatform(signature);
+    }
+    return signature;
+  }
 }
 
 (function(settings) {
 
+  const PlatformReform = new PlatformReformer();
+
   function getPlatformProjection() {
-    const browserSignature = getBrowserSignature();
-    return "Outkast";
+    const signature = internals.parseUserAgentSignature(navigator.userAgent);
+    return signature.platform;
   }
 
-  if (!settings.disabled) {
-    // https://stackoverflow.com/questions/38808968/change-navigator-platform-on-chrome-firefox-or-ie-to-test-os-detection-code
-    Object.defineProperty(navigator, "platform", {
+  const NOOP = () => {};
+
+  const navigatorPropertyMapping = {
+    'platform': getPlatformProjection,
+  };
+
+  // https://stackoverflow.com/questions/38808968/change-navigator-platform-on-chrome-firefox-or-ie-to-test-os-detection-code
+  const previous = {};
+  Object.keys(navigatorPropertyMapping).forEach(property => {
+    previous[property] = navigator[property];
+    Object.defineProperty(navigator, property, {
       get: function () {
-        return getPlatformProjection();
+        if (window.PlatformReformSettings && window.PlatformReformSettings.disabled) {
+          return previous[property];
+        } else {
+          return navigatorPropertyMapping[property]();
+        }
       },
-      set: function (value) {}
+      set: NOOP
      });
-  }
-})(window['platformReformSettings'] || {});
+  });
+})(window);
