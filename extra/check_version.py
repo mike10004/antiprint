feacdb0e6ee18602635761beeb5303226673c770
Mike Chaberski <mchaberski@gmail.com>
2017-10-27 15:30:27 -0400

    configure for crx artifact deployment

---
+++ extra/check_version.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import sys
+import json
+from argparse import ArgumentParser
+
+if __name__ == '__main__':
+    p = ArgumentParser()
+    p.add_argument('required_version')
+    p.add_argument('crx_manifest')
+    args = p.parse_args()
+    required_version = args.required_version
+    if args.required_version.endswith('-SNAPSHOT'):
+        required_version = required_version[:-len('-SNAPSHOT')]
+    with open(args.crx_manifest, 'r') as ifile:
+        manifest = json.load(ifile)
+    if manifest['version'] != required_version:
+        print('manifest version != pom version', file=sys.stderr)
+        exit(2)
---
+++ pack-extension.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+PROG="pack-extension"
+
+CHROME=$(which google-chrome || which chromium-browser)
+if [ -z "${CHROME}" ] ; then
+  echo "${PROG}: chrome is not installed or executable is not on PATH" >&2
+  exit 1
+fi
+
+KEYFILE=$1
+
+EXT_SRC_DIRNAME="antiprint-extension"
+DEFAULT_KEY="${PWD}/${EXT_SRC_DIRNAME}.pem"
+
+if [ -n "${KEYFILE}" -a -f "${KEYFILE}" ] ; then
+  KEYARG="--pack-extension-key=${KEYFILE}"
+else
+  if [ -f "${DEFAULT_KEY}" ] ; then
+    echo "${PROG}: reusing existing key ${DEFAULT_KEY}" >&2
+    KEYARG="--pack-extension-key=${DEFAULT_KEY}"
+  else
+    echo "${PROG}: new key file will be generated at ${DEFAULT_KEY}" >&2
+  fi
+fi
+
+rm -f "${EXT_SRC_DIRNAME}.crx"
+
+"${CHROME}" --disable-gpu --pack-extension="${PWD}/${EXT_SRC_DIRNAME}" "${KEYARG}"
+STATUS=$?
+if [ $STATUS -ne 0 ] ; then
+  echo "${PROG}: ${CHROME} exited with status ${STATUS}"
+  exit $STATUS
+fi
+
+if [ ! -f "${PWD}/${EXT_SRC_DIRNAME}.crx" ] ; then
+  echo "${PROG}: extension was not created but Chrome exited clean" >&2
+  exit 2
+fi
+
+echo "${PWD}/${EXT_SRC_DIRNAME}.crx"
