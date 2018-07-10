f50144cfccc32a89180591ca95c7bb81a04b6424
Mike Chaberski <mchaberski@gmail.com>
2018-07-10 15:03:59 -0400

    check javascript execution returns correct navigator properties

--- antiprint-e2e-tests/src/test/java/io/github/mike10004/antiprint/e2etests/PlatformProjectionTestBase.java
+++ antiprint-e2e-tests/src/test/java/io/github/mike10004/antiprint/e2etests/PlatformProjectionTestBase.java
@@ -1,5 +1,6 @@
 package io.github.mike10004.antiprint.e2etests;
 
+import com.google.common.base.Joiner;
 import com.google.common.io.CharSource;
 import com.google.common.io.Resources;
 import com.google.common.net.MediaType;
@@ -9,12 +10,19 @@ import io.github.mike10004.nanochamp.server.NanoServer;
 import net.sf.uadetector.OperatingSystemFamily;
 import net.sf.uadetector.UserAgentFamily;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import javax.annotation.Nullable;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -26,7 +34,7 @@ public abstract class PlatformProjectionTestBase extends BrowserUsingTestBase<We
             return userAgent.getFamily() == requiredUserAgentFamily
                     && userAgent.getOperatingSystem().getFamily() == requiredOsFamily;
         }).stream().findFirst().orElseThrow(() -> new IllegalArgumentException(requiredUserAgentFamily + "/" + requiredOsFamily));
-        System.out.println(navigator);
+        System.out.format("expected navigator: %s%n", Joiner.on(System.lineSeparator()).withKeyValueSeparator(" = ").join(navigator));
         String userAgent = navigator.get("userAgent").toString();
         byte[] html = Resources.toByteArray(getClass().getResource("/print-navigator.html"));
         NanoServer server = NanoServer.builder()
@@ -38,6 +46,13 @@ public abstract class PlatformProjectionTestBase extends BrowserUsingTestBase<We
             // the extension is only active if the page URL is http[s]
             try (NanoControl control = server.startServer()) {
                 driver.get(control.baseUri().toString());
+                Map<String, Object> jsResults = new HashMap<>();
+                navigator.keySet().forEach(navigatorPropertyName -> {
+                    String js = "return window.navigator." + navigatorPropertyName;
+                    Object navigatorPropertyValue = ((JavascriptExecutor)driver).executeScript(js);
+                    jsResults.put(navigatorPropertyName, navigatorPropertyValue);
+                });
+                assertNavigatorsEqual("js execution", navigator, jsResults);
                 /*
                  * Content is written with document.write, so we don't have to
                  * use a WebDriverWait to poll the page
@@ -49,16 +64,22 @@ public abstract class PlatformProjectionTestBase extends BrowserUsingTestBase<We
                 assertNotNull("div#content contents", json);
                 Map<String, Object> actual = Tests.navigatorObjectLoader().apply(CharSource.wrap(json));
                 maybePauseUntilKilled();
-                navigator.forEach((k, v) -> {
-                    System.out.format("%s = %s%n", k, actual.get(k));
-                    if (isImportant(k)) {
-                        assertEquals("property: " + k, navigator.get(k), actual.get(k));
-                    }
-                });
+                assertNavigatorsEqual("json on page", navigator, actual);
             }
         } finally {
             driver.quit();
         }
+    }
+
+    private static void assertNavigatorsEqual(String source, Map<String, Object> expected, Map<String, Object> actual) {
+        List<Map.Entry<String, Object>> mismatches = new ArrayList<>();
+        expected.forEach((k, v) -> {
+            System.out.format("%s = %s%n", k, actual.get(k));
+            if (isImportant(k) && !Objects.equals(expected.get(k), actual.get(k))) {
+                mismatches.add(actual.entrySet().stream().filter(e -> k.equals(e.getKey())).findFirst().orElseThrow(IllegalStateException::new));
+            }
+        });
+        assertEquals("expected zero navigator property mismatches in " + source, Collections.emptyList(), mismatches);
     }
 
     private static boolean isImportant(String navigatorProperty) {
