6fe558630dfea0711f16ece512e3b1f5ac1f54f9
Mike Chaberski <mchaberski@gmail.com>
2018-09-20 14:14:31 -0400

    start 0.11

--- antiprint-extension/src/test/other/check_version.py
+++ antiprint-extension/src/test/other/check_version.py
@@ -3,28 +3,14 @@
 from __future__ import print_function
 import sys
 import json
-import re
-import xml.etree.ElementTree as ElementTree
 from argparse import ArgumentParser
 
 ERR_VERSION_MISMATCH = 2
 
+
 def check_efw_pom(pom_file, required_version):
-    ns = {
-        'pom': 'http://maven.apache.org/POM/4.0.0'
-    }
-    tree = ElementTree.parse(pom_file)
-    root = tree.getroot()
-    version_el = root.findall('pom:version', ns)[0]
-    version = version_el.text
-    m = re.match(r'^\d+\.\d+\.\d+x(.+)', version)
-    if m is None:
-        print("extensible-firefox-webdriver pom version does not match expected pattern: {}".format(version), file=sys.stderr)
-        exit(ERR_VERSION_MISMATCH)
-    suffix = m.group(1)
-    if suffix != required_version:
-        print("extensible-firefox-webdriver pom version {} != extension artifact pom version {}".format(suffix, required_version), file=sys.stderr)
-        exit(ERR_VERSION_MISMATCH)
+    # Version is interpolated from ${project.parent.version}, so this check is no longer necessary
+    pass
 
 
 def check_manifest(crx_manifest, required_version):
@@ -34,7 +20,7 @@ def check_manifest(crx_manifest, required_version):
         manifest = json.load(ifile)
     manifest_version = manifest['version']
     if manifest_version != required_version:
-        print("manifest version (from %s) %s != required pom version %s" % (crx_manifest, manifest_version, required_version), file=sys.stderr)
+        print("manifest version (from %s) %s != required version %s (derived from pom.xml)" % (crx_manifest, manifest_version, required_version), file=sys.stderr)
         exit(ERR_VERSION_MISMATCH)
 
 
